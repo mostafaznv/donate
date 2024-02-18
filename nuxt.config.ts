b123f1078edab5d5d12675491ffe8583401ba622
mostafaznv <mostafa.zeinivand@gmail.com>
2024-02-19 01:56:56 +0330

    fix: prepend /donate to favicon urls

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,10 +16,10 @@ export default defineNuxtConfig({
             ],
             
             link: [
-                {rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png'},
-                {rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png'},
-                {rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png'},
-                {rel: 'manifest', href: '/favicon/site.webmanifest'}
+                {rel: 'apple-touch-icon', sizes: '180x180', href: '/donate/favicon/apple-touch-icon.png'},
+                {rel: 'icon', type: 'image/png', sizes: '32x32', href: '/donate/favicon/favicon-32x32.png'},
+                {rel: 'icon', type: 'image/png', sizes: '16x16', href: '/donate/favicon/favicon-16x16.png'},
+                {rel: 'manifest', href: '/donate/favicon/site.webmanifest'}
             ]
         }
     },
