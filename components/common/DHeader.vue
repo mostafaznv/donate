1c63dfddcff3eb6e83b40992e55f625b7c29be07
mostafaznv <mostafa.zeinivand@gmail.com>
2024-02-19 13:44:36 +0330

    feat: rearrange content

--- components/common/DHeader.vue
+++ components/common/DHeader.vue
@@ -2,8 +2,8 @@
     <v-app-bar color="transparent" :height="$vuetify.display.mobile ? 200 : 300">
         <v-container class="d-flex flex-column align-center justify-center">
             <div class="text-black text-h2 text-md-h1 text-center mt-2">
-                <div>Hi there!</div>
-                <span>I'm Mostafa Zeinivand</span>
+                <div>I'm Mostafa</div>
+                <span>Thank you for your support</span>
 
                 <a class="d-block mt-2" href="https://github.com/mostafaznv" target="_blank">
                     <d-github-icon width="28" height="28" />
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,5 @@
 <template>
-    <d-glow-card class="d-none d-md-block">
-        <div class="d-flex flex-column align-center justify-center w-100 py-8 py-md-12">
-            <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" class="text-surface-variant" fill="currentColor" viewBox="0 0 512 512">
-                <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />
-            </svg>
-
-            <h2 class="mb-6 mt-2">
-                Thank you for your support!
-            </h2>
-
-            <p class="text-center">
-                I believe in open source software and its power to change the world for the better. However, I also recognize that developing and maintaining high-quality open source projects requires significant time and effort. By supporting me, you are helping to ensure that this project remains healthy and active, and that it can continue to benefit people around the world.
-                <br /><br />
-                Your contribution, no matter how small, is deeply appreciated.
-            </p>
-        </div>
-    </d-glow-card>
-
-
-    <v-row class="mt-md-3">
+    <v-row class="mb-3">
         <v-col v-for="wallet in wallets" cols="12" md="4" :key="wallet.key">
             <d-glow-card @click="copy(wallet)" class="cursor-pointer" fill-height>
                 <div class="d-flex flex-column align-center justify-center text-center fill-height py-3">
@@ -50,6 +31,24 @@
             </d-glow-card>
         </v-col>
     </v-row>
+
+    <d-glow-card>
+        <div class="d-flex flex-column align-center justify-center w-100 py-8 py-md-12">
+            <!--<svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" class="text-surface-variant" fill="currentColor" viewBox="0 0 512 512">
+                <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />
+            </svg>
+
+            <h2 class="mb-6 mt-2">
+                Thank you for your support!
+            </h2>-->
+
+            <p class="text-center">
+                <span class="font-weight-bold">Your contribution, no matter how small, is deeply appreciated.</span>
+                <br /><br />
+                I believe in open source software and its power to change the world for the better. However, I also recognize that developing and maintaining high-quality open source projects requires significant time and effort. By supporting me, you are helping to ensure that this project remains healthy and active, and that it can continue to benefit people around the world.
+            </p>
+        </div>
+    </d-glow-card>
 </template>
 
 <script lang="ts" setup>
